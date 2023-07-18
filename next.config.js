b9d8ad161aec76da1e3167a159f625037f674e93
Bakon <julio.schilders@gmail.com>
2023-07-18 13:21:37 +0200

    add generateBuildid to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-}
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
+    generateBuildId: async () => {
+        // You can, for example, get the latest git commit hash here
+        return 'my-build-id';
+    },
+};
